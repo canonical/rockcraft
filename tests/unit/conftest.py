731c1a12b8789e41707c62cf33bfa6e7eb4615c0
Sergio Schvezov <sergio.schvezov@canonical.com>
2021-10-18 12:46:59 -0300

    bootstrap minimal command line interface
    Just enought to run "rockcraft pack"
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

---
+++ tests/unit/conftest.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright (C) 2021 Canonical Ltd
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from unittest.mock import patch
+
+import pytest
+
+from rockcraft import ui
+
+
+@pytest.fixture
+def emit_mock():
+    """Setup a fake emitter."""
+    patcher = patch.object(ui, "emit")
+    yield patcher.start()
+    patcher.stop()
