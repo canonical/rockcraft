f7c7cc8d31a05a35d5e074e17e3f3d24f8adbece
Sergio Schvezov <sergio.schvezov@canonical.com>
2021-10-18 15:18:18 -0300

    requirements: freeze new craft-cli
    Improve freezing logic to work with craft-cli from git.
    
    Signed-off-by: Sergio Schvezov <sergio.schvezov@canonical.com>

--- tools/freeze-requirements.sh
+++ tools/freeze-requirements.sh
@@ -5,10 +5,13 @@ venv_dir="$(mktemp -d)"
 python3 -m venv "$venv_dir"
 . "$venv_dir/bin/activate"
 
+pip install git+https://github.com/canonical/craft-cli.git@616673f9312338c841e0da5de6417056aac88aa5#egg=craft_cli
 pip install -e .
-pip freeze --exclude-editable > requirements.txt
+pip freeze --exclude-editable |grep -v ^craft-cli > requirements.txt
+echo "git+https://github.com/canonical/craft-cli.git@616673f9312338c841e0da5de6417056aac88aa5#egg=craft_cli" >> requirements.txt
 
 pip install -e .[dev]
-pip freeze --exclude-editable > requirements-dev.txt
+pip freeze --exclude-editable | grep -v ^craft-cli > requirements-dev.txt
+echo "git+https://github.com/canonical/craft-cli.git@616673f9312338c841e0da5de6417056aac88aa5#egg=craft_cli" >> requirements-dev.txt
 
 rm -rf "$venv_dir"
