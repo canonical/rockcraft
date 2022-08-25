d72ec96f68d099a8b816d2d8d655a35b045a6c43
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2022-08-25 09:14:36 +0200

    feat: refactor all errors into reusable module

--- rockcraft/project.py
+++ rockcraft/project.py
@@ -21,17 +21,9 @@ from typing import Any, Dict, List, Literal, Optional, Tuple
 import pydantic
 import spdx_license_list  # type: ignore
 import yaml
-from craft_cli.errors import CraftError
 
+from rockcraft.errors import ProjectLoadError, ProjectValidationError
 from rockcraft.parts import validate_part
-
-
-class ProjectLoadError(CraftError):
-    """Error loading rockcraft.yaml."""
-
-
-class ProjectValidationError(CraftError):
-    """Error validatiing rockcraft.yaml."""
 
 
 class Project(pydantic.BaseModel):
