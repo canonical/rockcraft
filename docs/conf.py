93aa165fd3b30f5ce83ad18d7b8b1eb14054aa19
Cristovao Cordeiro <cristovao.cordeiro@canonical.com>
2022-09-14 12:18:46 +0200

    fix: linting

--- docs/conf.py
+++ docs/conf.py
@@ -82,7 +82,7 @@ html_static_path = ["_static"]
 # These paths are either relative to html_static_path
 # or fully qualified paths (eg. https://...)
 html_css_files = [
-    'css/custom.css',
+    "css/custom.css",
 ]
 
 # Do (not) include module names.
