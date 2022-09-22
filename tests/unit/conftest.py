2f5b9c48321d3dce5dc0a2b17b0a27aceee6ec3c
Callahan <callahan.kovacs@canonical.com>
2022-09-22 11:57:56 -0500

    tests: test lifecycle.py::_run_in_provider (#83)
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -14,13 +14,51 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+import contextlib
+from pathlib import Path
 from unittest import mock
 
 import pytest
-from craft_providers import Executor
+from craft_providers import Executor, base
+
+from rockcraft.providers import Provider
 
 
 @pytest.fixture
 def mock_instance():
     """Provide a mock instance (Executor)."""
-    yield mock.Mock(spec=Executor)
+    _mock_instance = mock.Mock(spec=Executor)
+    yield _mock_instance
+
+
+@pytest.fixture(autouse=True)
+def fake_provider(mock_instance):
+    """Fixture to provide a minimal fake provider."""
+
+    class FakeProvider(Provider):
+        """Fake provider."""
+
+        def clean_project_environments(self, *, project_name: str, project_path: Path):
+            pass
+
+        @classmethod
+        def ensure_provider_is_available(cls) -> None:
+            pass
+
+        @classmethod
+        def is_provider_available(cls) -> bool:
+            return True
+
+        @contextlib.contextmanager
+        def launched_environment(
+            self,
+            *,
+            project_name: str,
+            project_path: Path,
+            base_configuration: base.Base,
+            build_base: str,
+            instance_name: str,
+        ):
+            yield mock_instance
+
+    return FakeProvider()
