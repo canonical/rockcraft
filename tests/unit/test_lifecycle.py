2f8e95c6f66a4929bf35300cf6b4a4a0f42b8b70
Callahan Kovacs <callahan.kovacs@canonical.com>
2022-09-27 11:51:14 -0500

    commands: add clean command
    Signed-off-by: Callahan Kovacs <callahan.kovacs@canonical.com>

--- tests/unit/test_lifecycle.py
+++ tests/unit/test_lifecycle.py
@@ -19,7 +19,7 @@ from pathlib import Path
 from unittest.mock import Mock, patch
 
 import pytest
-from craft_cli import EmitterMode, emit
+from craft_cli import CraftError, EmitterMode, emit
 from craft_providers.bases.buildd import BuilddBaseAlias
 
 from rockcraft import lifecycle
@@ -29,6 +29,12 @@ from rockcraft import lifecycle
 def mock_project():
     with patch("rockcraft.project") as _mock_project:
         _mock_project.name = "test-name"
+        _mock_project.platforms = {
+            "test-platform": {
+                "build_on": ["test-build-on"],
+                "build_for": ["test-build-for"],
+            }
+        }
         yield _mock_project
 
 
@@ -37,6 +43,76 @@ def mock_provider(mocker, mock_instance, fake_provider):
     _mock_provider = Mock(wraps=fake_provider)
     mocker.patch("rockcraft.lifecycle.get_provider", return_value=_mock_provider)
     yield _mock_provider
+
+
+@pytest.fixture()
+def mock_get_instance_name(mocker):
+    with patch(
+        "rockcraft.lifecycle.get_instance_name", return_value="test-instance-name"
+    ) as _mock_get_instance_name:
+        yield _mock_get_instance_name
+
+
+@pytest.mark.parametrize(
+    "command_name", ["pull", "overlay", "build", "stage", "prime", "pack"]
+)
+def test_run_run_in_provider(command_name, mocker, mock_project):
+    """Verify `run()` calls `run_in_provider()` when not in managed or destructive
+    mode.
+    """
+    mocker.patch("rockcraft.lifecycle.load_project", return_value=mock_project)
+    mocker.patch("rockcraft.lifecycle.utils.is_managed_mode", return_value=False)
+    mock_run_in_provider = mocker.patch("rockcraft.lifecycle.run_in_provider")
+    lifecycle.run(command_name=command_name, parsed_args=argparse.Namespace())
+
+    mock_run_in_provider.assert_called_once_with(
+        mock_project, command_name, argparse.Namespace()
+    )
+
+
+def test_run_clean_provider_error(mocker, mock_project):
+    """Verify `run()` calls `clean_provider()` when not in managed or destructive
+    mode.
+    """
+    mocker.patch("rockcraft.lifecycle.load_project", return_value=mock_project)
+    mocker.patch("rockcraft.lifecycle.utils.is_managed_mode", return_value=False)
+
+    with pytest.raises(CraftError) as error:
+        lifecycle.run(command_name="clean", parsed_args=argparse.Namespace())
+
+    assert str(error.value) == "`rockcraft clean` for an environment is not supported"
+
+
+def test_run_clean_part_error(mocker, mock_project):
+    """Verify cleaning a part raises an error."""
+    mocker.patch("rockcraft.lifecycle.load_project", return_value=mock_project)
+    mocker.patch("rockcraft.lifecycle.utils.is_managed_mode", return_value=True)
+    mocker.patch(
+        "rockcraft.lifecycle.oci.Image.from_docker_registry",
+        return_value=(Mock(), Mock()),
+    )
+
+    with pytest.raises(CraftError) as error:
+        lifecycle.run(
+            command_name="clean", parsed_args=argparse.Namespace(parts="test-part")
+        )
+
+    assert str(error.value) == "`rockcraft clean <part-name>` is not supported"
+
+
+def test_run_clean_destructive_mode_error(mocker, mock_project):
+    """Verify cleaning in destructive mode raises an error."""
+    mocker.patch("rockcraft.lifecycle.load_project", return_value=mock_project)
+    mocker.patch("rockcraft.lifecycle.utils.is_managed_mode", return_value=True)
+    mocker.patch(
+        "rockcraft.lifecycle.oci.Image.from_docker_registry",
+        return_value=(Mock(), Mock()),
+    )
+
+    with pytest.raises(CraftError) as error:
+        lifecycle.run(command_name="clean", parsed_args=argparse.Namespace())
+
+    assert str(error.value) == "`rockcraft clean` in destructive mode is not supported"
 
 
 @pytest.mark.parametrize(
@@ -58,6 +134,7 @@ def mock_provider(mocker, mock_instance, fake_provider):
     ],
 )
 def test_lifecycle_run_in_provider(
+    mock_get_instance_name,
     mock_instance,
     mock_provider,
     mock_project,
@@ -72,9 +149,6 @@ def test_lifecycle_run_in_provider(
     mock_get_base_configuration = mocker.patch(
         "rockcraft.lifecycle.get_base_configuration",
         return_value=mock_base_configuration,
-    )
-    mock_get_instance_name = mocker.patch(
-        "rockcraft.lifecycle.get_instance_name", return_value="test-instance-name"
     )
     mock_capture_logs_from_instance = mocker.patch(
         "rockcraft.lifecycle.capture_logs_from_instance"
