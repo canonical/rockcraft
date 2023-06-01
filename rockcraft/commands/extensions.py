b8149071aa84a8fc26649977fa67cb8ba9ba1d59
Tiago Nobrega <tiago.nobrega@canonical.com>
2023-06-01 17:32:40 -0300

    extensions: add cli commands
    The commands are 'list-extensions' and 'expand-extensions' and they
    have the same semantics as the Snapcraft equivalents. Some minor
    refactoring to re-use testing data accross multiple tests.
    
    Fixes #274

---
+++ rockcraft/commands/extensions.py
@@ -0,0 +1,103 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2023 Canonical Ltd.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License version 3 as
+# published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Snapcraft lifecycle commands."""
+
+import abc
+import argparse
+import textwrap
+from pathlib import Path
+from typing import Dict, List
+
+import tabulate
+from craft_cli import BaseCommand, emit
+from overrides import overrides
+from pydantic import BaseModel
+
+from rockcraft import extensions
+from rockcraft.project import load_project
+
+
+class ExtensionModel(BaseModel):
+    """Extension model for presentation."""
+
+    name: str
+    bases: List[str]
+
+    def marshal(self) -> Dict[str, str]:
+        """Marshal model into a dictionary for presentation."""
+        return {
+            "Extension name": self.name,
+            "Supported bases": ", ".join(sorted(self.bases)),
+        }
+
+
+class ListExtensionsCommand(BaseCommand, abc.ABC):
+    """List available extensions for all supported bases."""
+
+    name = "list-extensions"
+    help_msg = "List available extensions for all supported bases."
+    overview = textwrap.dedent(
+        """
+        List available extensions and their corresponding bases.
+        """
+    )
+
+    @overrides
+    def run(self, parsed_args: argparse.Namespace):
+        """Print the list of available extensions and their bases."""
+        extension_presentation: Dict[str, ExtensionModel] = {}
+
+        # New extensions.
+        for extension_name in extensions.registry.get_extension_names():
+            extension_class = extensions.registry.get_extension_class(extension_name)
+            extension_bases = list(extension_class.get_supported_bases())
+            extension_presentation[extension_name] = ExtensionModel(
+                name=extension_name, bases=extension_bases
+            )
+
+        printable_extensions = sorted(
+            [v.marshal() for v in extension_presentation.values()],
+            key=lambda d: d["Extension name"],
+        )
+        emit.message(tabulate.tabulate(printable_extensions, headers="keys"))
+
+
+class ExtensionsCommand(ListExtensionsCommand, abc.ABC):
+    """A command alias to list the available extensions."""
+
+    name = "extensions"
+    hidden = True
+
+
+class ExpandExtensionsCommand(BaseCommand, abc.ABC):
+    """Expand the extensions in the snapcraft.yaml file."""
+
+    name = "expand-extensions"
+    help_msg = "Expand extensions in snapcraft.yaml"
+    overview = textwrap.dedent(
+        """
+        Extensions listed rockcraft.yaml will be
+        expanded and shown as output.
+        """
+    )
+
+    @overrides
+    def run(self, parsed_args: argparse.Namespace):
+        """Print the project's specification with the extensions expanded."""
+        project = load_project(Path("rockcraft.yaml"))
+
+        emit.message(project.to_yaml())
