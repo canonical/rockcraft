360087de07a02478dafb7a02fa6e0cda517e99b0
Sheng Yu <sheng.yu@canonical.com>
2023-12-01 13:43:12 -0500

    chore(linter): fix mypy annotation errors

--- rockcraft/cli.py
+++ rockcraft/cli.py
@@ -17,11 +17,15 @@
 """Command-line application entry point."""
 
 import logging
+from typing import TYPE_CHECKING
 
 from rockcraft import plugins
 
 from . import commands
 from .services import RockcraftServiceFactory
+
+if TYPE_CHECKING:
+    from .application import Rockcraft
 
 
 def run() -> int:
@@ -39,7 +43,7 @@ def run() -> int:
     return app.run()
 
 
-def _create_app():  # noqa: ANN202
+def _create_app() -> "Rockcraft":
     # pylint: disable=import-outside-toplevel
     # Import these here so that the script that generates the docs for the
     # commands doesn't need to know *too much* of the application.
