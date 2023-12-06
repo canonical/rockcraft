7955ba06e6f329cba01ac7d8fd75568992865be1
Tiago Nobrega <tiago.nobrega@canonical.com>
2023-12-06 16:19:22 -0300

    fix: use correct error message for invalid version
    The code refers to the 'name' message, but the bug was due to the
    "error_msg_templates" in the Config: it made *every* regex validation
    failure use the INVALID_NAME_MESSAGE message, even for those fields that
    *aren't* the 'name'.
    
    Since the "error_msg_templates" option is deprecated and gone in
    Pydantic 2 anyway, remove it altogether and use the new
    transform_pydantic_error() hook from craft-application to preserve the
    customisation of the 'name' field's message. This is necessary because
    our 'name' regex is different than the 'name' regex in
    craft-application's model (ours is a bit stricter), so we can't just use
    craft-application's message as that would be misleading.
    
    Fixes #429

--- tests/unit/test_project.py
+++ tests/unit/test_project.py
@@ -433,6 +433,23 @@ def test_project_name_invalid(yaml_loaded_data, invalid_name):
     assert expected_message in str(err.value)
 
 
+def test_project_version_invalid(yaml_loaded_data):
+    yaml_loaded_data["version"] = "invalid_version"
+
+    with pytest.raises(CraftValidationError) as err:
+        load_project_yaml(yaml_loaded_data)
+
+    # We don't want to be completely tied to the formatting of the message here,
+    # because it comes from craft-application and might change slightly. We just
+    # want to ensure that the message refers to the version.
+    expected_contents = "Invalid version: Valid versions consist of"
+    expected_suffix = "(in field 'version')"
+
+    message = str(err.value)
+    assert expected_contents in message
+    assert message.endswith(expected_suffix)
+
+
 @pytest.mark.parametrize(
     "field", ["name", "version", "base", "parts", "description", "summary", "license"]
 )
