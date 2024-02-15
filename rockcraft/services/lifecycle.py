7ef05c660bfd9ef32c331967688c2fafb6abc8fe
Tiago Nobrega <tiago.nobrega@canonical.com>
2024-02-15 17:45:57 -0300

    refactor: update & remove duplicated code
    This cleans-up the code related to functionality that is now implemented
    upstream. This includes:
    
    - registering application plugins;
    - package-repositories installation (including overlays).
    
    It also updates some tests to latest features, like "parallel_build_count".

--- rockcraft/services/lifecycle.py
+++ rockcraft/services/lifecycle.py
@@ -15,17 +15,12 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """Rockcraft Lifecycle service."""
-
-import contextlib
 from pathlib import Path
-from typing import Any, cast
+from typing import cast
 
 from craft_application import LifecycleService
-from craft_archives import repo  # type: ignore[import-untyped]
-from craft_cli import emit
-from craft_parts import Features, LifecycleManager, Step, callbacks
-from craft_parts.errors import CallbackRegistrationError
-from craft_parts.infos import ProjectInfo, StepInfo
+from craft_parts import Features
+from craft_parts.infos import StepInfo
 from overrides import override  # type: ignore[reportUnknownVariableType]
 
 from rockcraft import layers
@@ -47,7 +42,6 @@ class RockcraftLifecycleService(LifecycleService):
 
         # Configure extra args to the LifecycleManager
         project = cast(Project, self._project)
-        project_vars = {"version": project.version}
 
         services = cast(RockcraftServiceFactory, self._services)
         image_service = services.image
@@ -57,72 +51,25 @@ class RockcraftLifecycleService(LifecycleService):
             base_layer_dir=image_info.base_layer_dir,
             base_layer_hash=image_info.base_digest,
             base=project.base,
-            package_repositories=project.package_repositories or [],
             project_name=project.name,
-            project_vars=project_vars,
             rootfs_dir=image_info.base_layer_dir,
         )
 
         super().setup()
 
     @override
-    def run(self, step_name: str | None, part_names: list[str] | None = None) -> None:
-        """Run the lifecycle manager for the parts."""
-        # Overridden to configure package repositories.
-        project = cast(Project, self._project)
-        package_repositories = project.package_repositories
+    def post_prime(self, step_info: StepInfo) -> bool:
+        """Perform base-layer pruning on primed files."""
+        prime_dir = step_info.prime_dir
+        base_layer_dir = step_info.rootfs_dir
+        files: set[str]
 
-        if package_repositories is not None:
-            _install_package_repositories(package_repositories, self._lcm)
-            with contextlib.suppress(CallbackRegistrationError):
-                callbacks.register_configure_overlay(_install_overlay_repositories)
+        files = step_info.state.files if step_info.state else set()
+        layers.prune_prime_files(prime_dir, files, base_layer_dir)
 
-        try:
-            callbacks.register_post_step(_post_prime_callback, step_list=[Step.PRIME])
-            super().run(step_name, part_names)
-        finally:
-            callbacks.unregister_all()
+        _python_usrmerge_fix(step_info)
 
-
-def _install_package_repositories(
-    package_repositories: list[dict[str, Any]] | None,
-    lifecycle_manager: LifecycleManager,
-) -> None:
-    """Install package repositories in the environment."""
-    if not package_repositories:
-        emit.debug("No package repositories specified, none to install.")
-        return
-
-    refresh_required = repo.install(package_repositories, key_assets=Path("/dev/null"))
-    if refresh_required:
-        emit.progress("Refreshing repositories")
-        lifecycle_manager.refresh_packages_list()
-
-    emit.progress("Package repositories installed")
-
-
-def _install_overlay_repositories(overlay_dir: Path, project_info: ProjectInfo) -> None:
-    if project_info.base != "bare":
-        package_repositories = project_info.package_repositories
-        repo.install_in_root(
-            project_repositories=package_repositories,
-            root=overlay_dir,
-            key_assets=Path("/dev/null"),
-        )
-
-
-def _post_prime_callback(step_info: StepInfo) -> bool:
-    prime_dir = step_info.prime_dir
-    base_layer_dir = step_info.rootfs_dir
-    files: set[str]
-
-    files = step_info.state.files if step_info.state else set()
-
-    layers.prune_prime_files(prime_dir, files, base_layer_dir)
-
-    _python_usrmerge_fix(step_info)
-
-    return True
+        return True
 
 
 def _python_usrmerge_fix(step_info: StepInfo):
