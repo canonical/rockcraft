276e66914ac9edbd56c35afb6f7c9f631a6a6959
Vladimir Petko <vladimir.petko@canonical.com>
2024-08-28 16:03:38 +1200

    feat: jlink plugin
    Add initial implementation of jlink plugin.
    It creates Java runtime for a built project.
    If no jar files are found it generates an image for java.base
    module.

---
+++ rockcraft/plugins/jlink_plugin.py
@@ -0,0 +1,88 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2024 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""The Java runtime plugin."""
+
+from overrides import override
+
+from typing import Any, Dict, Literal, List, Set, cast
+
+from craft_parts.plugins import Plugin, PluginProperties
+
+
+class JLinkPluginProperties(PluginProperties):
+    """The part properties used by the JLink plugin."""
+    plugin: Literal["jlink"] = "jlink"
+    jlink_java_version: int = 21
+    jlink_jars: list[str] = []
+
+
+class JLinkPlugin(Plugin):
+    """Create a Java Runtime using JLink"""
+
+    properties_class = JLinkPluginProperties
+
+    @override
+    def get_build_packages(self) -> Set[str]:
+        options = cast(JLinkPluginProperties, self._options)
+        return {f"openjdk-{options.jlink_java_version}-jdk"}
+
+    @override
+    def get_build_environment(self) -> Dict[str, str]:
+        return {}
+
+    @override
+    def get_build_snaps(self) -> Set[str]:
+        return ["chisel"]
+
+    @override
+    def get_build_commands(self) -> List[str]:
+        """Return a list of commands to run during the build step."""
+        options = cast(JLinkPluginProperties, self._options)
+
+        commands = []
+        if hasattr(options, "source") and isinstance(options.source, str):
+            commands.append("chisel cut --release `pwd` --root ${CRAFT_PART_INSTALL} base-files_base openjdk-"+str(options.jlink_java_version)+"-jre-headless_headless")
+        else:
+            commands.append("chisel cut --root ${CRAFT_PART_INSTALL} base-files_base openjdk-" + str(options.jlink_java_version) + "-jre-headless_headless")
+
+
+        if len(options.jlink_jars) > 0:
+            jars = " ".join(["${CRAFT_STAGE}/" + x  for x in  options.jlink_jars])
+            commands.append(f"PROCESS_JARS={jars}")
+        else:
+            commands.append("PROCESS_JARS=$(find ${CRAFT_STAGE}/jars -type f -name *.jar)")
+
+        # create temp folder
+        commands.append("mkdir -p ${CRAFT_PART_BUILD}/tmp")
+        # extract jar files into temp folder
+        commands.append("(cd ${CRAFT_PART_BUILD}/tmp && for jar in ${PROCESS_JARS}; do jar xvf ${jar}; done;)")
+        commands.append("cpath=$(find ${CRAFT_PART_BUILD}/tmp -type f -name *.jar)")
+        commands.append("cpath=$(echo ${cpath} | sed s'/[[:space:]]/:/'g)")
+        commands.append("echo ${cpath}")
+        commands.append("if [ \"x${PROCESS_JARS}\" == \"x\" ]; then deps=$(jdeps --class-path=${cpath} -q --recursive  --ignore-missing-deps --print-module-deps --multi-release 21 ${PROCESS_JARS}; else deps=java.base; fi)")
+        commands.append("install_root=${CRAFT_PART_INSTALL}/usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/")
+
+        commands.append("rm -rf ${install_root} && jlink --add-modules ${deps} --output ${install_root}")
+        # create /usr/bin/java link
+        commands.append("(cd ${CRAFT_PART_INSTALL} && mkdir -p usr/bin && ln -s --relative usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/bin/java usr/bin/)")
+        commands.append("mkdir -p ${CRAFT_PART_INSTALL}/etc/ssl/certs/java/")
+        # link cacerts
+        commands.append("cp /etc/ssl/certs/java/cacerts ${CRAFT_PART_INSTALL}/etc/ssl/certs/java/cacerts")
+        commands.append("cd ${CRAFT_PART_INSTALL}")
+        commands.append("rm -f usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/lib/security/cacerts")
+        commands.append("ln -s --relative etc/ssl/certs/java/cacerts usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/lib/security/cacerts")
+        return commands
---
+++ tests/unit/plugins/test_jlink_plugin.py
@@ -0,0 +1,91 @@
+# -*- Mode:Python; indent-tabs-mode:nil; tab-width:4 -*-
+#
+# Copyright 2024 Canonical Ltd.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License version 3 as published by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from pathlib import Path
+
+import pytest
+from craft_parts.infos import PartInfo, ProjectInfo
+from craft_parts.parts import Part
+from rockcraft.plugins.jlink_plugin import JLinkPlugin
+
+
+@pytest.fixture()
+def setup_method_fixture():
+    def _setup_method_fixture(new_dir, properties=None):
+        if properties is None:
+            properties = {}
+        plugin_properties = JLinkPlugin.properties_class.unmarshal(properties)
+        part = Part("foo", {})
+
+        project_info = ProjectInfo(application_name="test", cache_dir=new_dir)
+        project_info._parallel_build_count = 42
+
+        part_info = PartInfo(project_info=project_info, part=part)
+        part_info._part_install_dir = Path("install/dir")
+
+        return JLinkPlugin(properties=plugin_properties, part_info=part_info)
+
+    return _setup_method_fixture
+
+
+class TestPluginJLinkPlugin:
+    """JLink plugin tests."""
+
+    def test_get_build_snaps(self, setup_method_fixture, new_dir):
+        plugin = setup_method_fixture(new_dir)
+        assert plugin.get_build_snaps() == ["chisel"]
+
+    def test_get_build_packages_default(self, setup_method_fixture, new_dir):
+        plugin = setup_method_fixture(new_dir)
+        assert plugin.get_build_packages() == {
+            "openjdk-21-jdk"
+        }
+
+    def test_get_build_packages_17(self, setup_method_fixture, new_dir):
+        plugin = setup_method_fixture(new_dir, properties={"jlink-java-version": "17"})
+        assert plugin.get_build_packages() == {
+            "openjdk-17-jdk"
+        }
+
+    def test_get_build_environment(self, setup_method_fixture, new_dir):
+        plugin = setup_method_fixture(new_dir)
+
+        assert plugin.get_build_environment() == {}
+
+    def test_get_build_commands_default(self, setup_method_fixture, new_dir):
+        plugin = setup_method_fixture(new_dir)
+
+        commands = plugin.get_build_commands()
+        assert "chisel cut --root ${CRAFT_PART_INSTALL} base-files_base openjdk-21-jre-headless_headless" in commands
+        assert "PROCESS_JARS=$(find ${CRAFT_STAGE}/jars -type f -name *.jar)" in commands
+
+    def test_get_build_commands_17(self, setup_method_fixture, new_dir):
+        plugin = setup_method_fixture(new_dir, properties={"jlink-java-version": "17"})
+
+        commands = plugin.get_build_commands()
+        assert "chisel cut --root ${CRAFT_PART_INSTALL} base-files_base openjdk-17-jre-headless_headless" in commands
+
+    def test_get_build_commands_chisel_source(self, setup_method_fixture, new_dir):
+        plugin = setup_method_fixture(new_dir, properties={"source": "foo"})
+
+        commands = plugin.get_build_commands()
+        assert "chisel cut --release `pwd` --root ${CRAFT_PART_INSTALL} base-files_base openjdk-21-jre-headless_headless" in commands
+        assert "PROCESS_JARS=$(find ${CRAFT_STAGE}/jars -type f -name *.jar)" in commands
+
+
+    def test_get_build_commands_jars(self, setup_method_fixture, new_dir):
+        plugin = setup_method_fixture(new_dir, properties={"jlink-jars": ["foo.jar"]})
+        assert "PROCESS_JARS=${CRAFT_STAGE}/foo.jar" in plugin.get_build_commands()
