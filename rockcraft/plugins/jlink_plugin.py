b7654fabb22ada409b1d6b8ffe28b95fca41196c
Vladimir Petko <vladimir.petko@canonical.com>
2024-08-29 11:19:30 +1200

    lint: jlink_plugin.py - reformat

--- rockcraft/plugins/jlink_plugin.py
+++ rockcraft/plugins/jlink_plugin.py
@@ -16,22 +16,22 @@
 
 """The Java runtime plugin."""
 
-from overrides import override
-
-from typing import Any, Dict, Literal, List, Set, cast
+from typing import Dict, List, Literal, Set, cast
 
 from craft_parts.plugins import Plugin, PluginProperties
+from overrides import override
 
 
 class JLinkPluginProperties(PluginProperties):
     """The part properties used by the JLink plugin."""
+
     plugin: Literal["jlink"] = "jlink"
     jlink_java_version: int = 21
     jlink_jars: list[str] = []
 
 
 class JLinkPlugin(Plugin):
-    """Create a Java Runtime using JLink"""
+    """Create a Java Runtime using JLink."""
 
     properties_class = JLinkPluginProperties
 
@@ -55,34 +55,59 @@ class JLinkPlugin(Plugin):
 
         commands = []
         if hasattr(options, "source") and isinstance(options.source, str):
-            commands.append("chisel cut --release `pwd` --root ${CRAFT_PART_INSTALL} base-files_base openjdk-"+str(options.jlink_java_version)+"-jre-headless_headless")
+            commands.append(
+                "chisel cut --release ./ --root ${CRAFT_PART_INSTALL} base-files_base openjdk-"
+                + str(options.jlink_java_version)
+                + "-jre-headless_headless"
+            )
         else:
-            commands.append("chisel cut --root ${CRAFT_PART_INSTALL} base-files_base openjdk-" + str(options.jlink_java_version) + "-jre-headless_headless")
-
+            commands.append(
+                "chisel cut --root ${CRAFT_PART_INSTALL} base-files_base openjdk-"
+                + str(options.jlink_java_version)
+                + "-jre-headless_headless"
+            )
 
         if len(options.jlink_jars) > 0:
-            jars = " ".join(["${CRAFT_STAGE}/" + x  for x in  options.jlink_jars])
+            jars = " ".join(["${CRAFT_STAGE}/" + x for x in options.jlink_jars])
             commands.append(f"PROCESS_JARS={jars}")
         else:
-            commands.append("PROCESS_JARS=$(find ${CRAFT_STAGE}/jars -type f -name *.jar)")
+            commands.append(
+                "PROCESS_JARS=$(find ${CRAFT_STAGE}/jars -type f -name *.jar)"
+            )
 
         # create temp folder
         commands.append("mkdir -p ${CRAFT_PART_BUILD}/tmp")
         # extract jar files into temp folder
-        commands.append("(cd ${CRAFT_PART_BUILD}/tmp && for jar in ${PROCESS_JARS}; do jar xvf ${jar}; done;)")
+        commands.append(
+            "(cd ${CRAFT_PART_BUILD}/tmp && for jar in ${PROCESS_JARS}; do jar xvf ${jar}; done;)"
+        )
         commands.append("cpath=$(find ${CRAFT_PART_BUILD}/tmp -type f -name *.jar)")
         commands.append("cpath=$(echo ${cpath} | sed s'/[[:space:]]/:/'g)")
         commands.append("echo ${cpath}")
-        commands.append("if [ \"x${PROCESS_JARS}\" == \"x\" ]; then deps=$(jdeps --class-path=${cpath} -q --recursive  --ignore-missing-deps --print-module-deps --multi-release 21 ${PROCESS_JARS}; else deps=java.base; fi)")
-        commands.append("install_root=${CRAFT_PART_INSTALL}/usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/")
+        commands.append(
+            'if [ "x${PROCESS_JARS}" == "x" ]; then deps=$(jdeps --class-path=${cpath} -q --recursive  --ignore-missing-deps --print-module-deps --multi-release 21 ${PROCESS_JARS}; else deps=java.base; fi)'
+        )
+        commands.append(
+            "install_root=${CRAFT_PART_INSTALL}/usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/"
+        )
 
-        commands.append("rm -rf ${install_root} && jlink --add-modules ${deps} --output ${install_root}")
+        commands.append(
+            "rm -rf ${install_root} && jlink --add-modules ${deps} --output ${install_root}"
+        )
         # create /usr/bin/java link
-        commands.append("(cd ${CRAFT_PART_INSTALL} && mkdir -p usr/bin && ln -s --relative usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/bin/java usr/bin/)")
+        commands.append(
+            "(cd ${CRAFT_PART_INSTALL} && mkdir -p usr/bin && ln -s --relative usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/bin/java usr/bin/)"
+        )
         commands.append("mkdir -p ${CRAFT_PART_INSTALL}/etc/ssl/certs/java/")
         # link cacerts
-        commands.append("cp /etc/ssl/certs/java/cacerts ${CRAFT_PART_INSTALL}/etc/ssl/certs/java/cacerts")
+        commands.append(
+            "cp /etc/ssl/certs/java/cacerts ${CRAFT_PART_INSTALL}/etc/ssl/certs/java/cacerts"
+        )
         commands.append("cd ${CRAFT_PART_INSTALL}")
-        commands.append("rm -f usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/lib/security/cacerts")
-        commands.append("ln -s --relative etc/ssl/certs/java/cacerts usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/lib/security/cacerts")
+        commands.append(
+            "rm -f usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/lib/security/cacerts"
+        )
+        commands.append(
+            "ln -s --relative etc/ssl/certs/java/cacerts usr/lib/jvm/java-21-openjdk-${CRAFT_TARGET_ARCH}/lib/security/cacerts"
+        )
         return commands
