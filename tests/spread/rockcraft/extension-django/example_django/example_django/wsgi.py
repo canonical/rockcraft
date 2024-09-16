52bec700641d6f0e5a8db00cf36c75616225439c
Javier de la Puente <javier.delapuente@canonical.com>
2024-09-16 11:30:40 +0200

    add ignore imports in spread test for Django

--- tests/spread/rockcraft/extension-django/example_django/example_django/wsgi.py
+++ tests/spread/rockcraft/extension-django/example_django/example_django/wsgi.py
@@ -9,7 +9,9 @@ https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
 
 import os
 
-from django.core.wsgi import get_wsgi_application
+from django.core.wsgi import (  # pyright: ignore[reportMissingImports]
+    get_wsgi_application,
+)
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "example_django.settings")
 
