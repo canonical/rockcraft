22d18eaa24b844cf63714dea23f9a69c1f0d6159
ali ugur <ali.ugur@canonical.com>
2024-12-04 13:40:24 +0300

    Chore(test): Removed unnecessary django files

--- tests/data/django/test_name/test_name/urls.py
+++
@@ -1,7 +0,0 @@
-# Rockcraft repo doesn't need Django installed so ignore module-not-found mypy error
-from django.contrib import admin  # type: ignore
-from django.urls import path  # type: ignore
-
-urlpatterns = [
-    path("admin/", admin.site.urls),
-]
