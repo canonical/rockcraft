6030895d680fdc4f52d638365e7c4c6566291966
Tiago Nobrega <tiago.nobrega@canonical.com>
2024-12-06 09:04:21 -0300

    chore: drop uses of distutils
    The package is gone in Python 3.12. This only affected tests and unused code.

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -15,7 +15,6 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from pathlib import Path
-from unittest.mock import call
 
 import pytest
 from rockcraft import utils
@@ -58,51 +57,3 @@ def test_get_managed_environment_snap_channel_none(monkeypatch):
     monkeypatch.delenv("ROCKCRAFT_INSTALL_SNAP_CHANNEL", raising=False)
 
     assert utils.get_managed_environment_snap_channel() is None
-
-
-def test_confirm_with_user_defaults_with_tty(mock_input, mock_isatty):
-    mock_input.return_value = ""
-    mock_isatty.return_value = True
-
-    assert utils.confirm_with_user("prompt", default=True) is True
-    assert mock_input.mock_calls == [call("prompt [Y/n]: ")]
-    mock_input.reset_mock()
-
-    assert utils.confirm_with_user("prompt", default=False) is False
-    assert mock_input.mock_calls == [call("prompt [y/N]: ")]
-
-
-def test_confirm_with_user_defaults_without_tty(mock_input, mock_isatty):
-    mock_isatty.return_value = False
-
-    assert utils.confirm_with_user("prompt", default=True) is True
-    assert utils.confirm_with_user("prompt", default=False) is False
-
-    assert mock_input.mock_calls == []
-
-
-@pytest.mark.parametrize(
-    ("user_input", "expected"),
-    [
-        ("y", True),
-        ("Y", True),
-        ("yes", True),
-        ("YES", True),
-        ("n", False),
-        ("N", False),
-        ("no", False),
-        ("NO", False),
-    ],
-)
-def test_confirm_with_user(user_input, expected, mock_input, mock_isatty):
-    mock_input.return_value = user_input
-
-    assert utils.confirm_with_user("prompt") == expected
-    assert mock_input.mock_calls == [call("prompt [y/N]: ")]
-
-
-def test_confirm_with_user_errors_in_managed_mode(mock_is_managed_mode):
-    mock_is_managed_mode.return_value = True
-
-    with pytest.raises(RuntimeError):
-        utils.confirm_with_user("prompt")
