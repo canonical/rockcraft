97e8bcb0b459228186b16b3c16a8f5c7bfe0dbfc
ali ugur <ali.ugur@canonical.com>
2024-12-13 10:11:42 +0300

    chore(): Revert version change in statsd

--- rockcraft/extensions/gunicorn.py
+++ rockcraft/extensions/gunicorn.py
@@ -91,7 +91,7 @@ class _GunicornBase(Extension):
             },
             f"{self.framework}-framework/statsd-exporter": {
                 "build-snaps": ["go"],
-                "source-tag": "v0.26.1",
+                "source-tag": "v0.26.0",
                 "plugin": "go",
                 "source": "https://github.com/prometheus/statsd_exporter.git",
             },
