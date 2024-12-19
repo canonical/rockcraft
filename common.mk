77e4f4af620aba64afb181618a54349a043f25ab
Tiago Nobrega <tiago.nobrega@canonical.com>
2024-12-19 18:11:17 -0300

    chore: update sphinx-lint call

--- common.mk
+++ common.mk
@@ -152,7 +152,7 @@ lint-docs:  ##- Lint the documentation
 ifneq ($(CI),)
 	@echo ::group::$@
 endif
-	uv run --extra docs sphinx-lint --max-line-length 88 --enable all $(DOCS)
+	uv run --extra docs sphinx-lint --max-line-length 80 --ignore docs/reference/commands/ --ignore docs/sphinx-starter-pack/ --ignore docs/_build/ --enable all $(DOCS)
 ifneq ($(CI),)
 	@echo ::endgroup::
 endif
