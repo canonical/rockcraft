1fde7eb295ffce90c4ebdb39d1e7f6437c0909cc
Ali Ugur <ali.ugur@canonical.com>
2025-03-20 08:57:26 +0300

    Chore(): Fix Go extension non-root

--- rockcraft/extensions/go.py
+++ rockcraft/extensions/go.py
@@ -65,11 +65,9 @@ class GoFramework(Extension):
             "go-framework/base-layout": {
                 "plugin": "nil",
                 "override-build": "mkdir -p ${CRAFT_PART_INSTALL}/app",
-            },
-            "go-framework/install-app": {
-                **self._get_install_app_part(),
                 "permissions": [{"owner": 584792, "group": 584792}],
             },
+            "go-framework/install-app": self._get_install_app_part(),
             "go-framework/runtime": {
                 "plugin": "nil",
                 "stage-packages": ["ca-certificates_data"],
