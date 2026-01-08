e81fbfae9aad8fdbd14949632cf82ef6aa649888
Anna Sancho Vaquer <anna.sanchovaquer@canonical.com>
2026-01-08 09:38:23 +0100

    docs: using intersphinx for links
    Signed-off-by: Anna Sancho Vaquer <anna.sanchovaquer@canonical.com>

--- docs/conf.py
+++ docs/conf.py
@@ -133,6 +133,7 @@ intersphinx_mapping = {
     ),
     "charmcraft": ("https://documentation.ubuntu.com/charmcraft/stable/", None),
     "pebble": ("https://documentation.ubuntu.com/pebble", None),
+    "slice definitions": ("https://documentation.ubuntu.com/chisel/en/latest/reference/chisel-releases/slice-definitions/", None),
 }
 # See also:
 # https://www.sphinx-doc.org/en/master/usage/extensions/intersphinx.html#confval-intersphinx_disabled_reftypes
