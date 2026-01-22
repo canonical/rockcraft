5e1e3471525e1abd5ca64bf7a4c29822595ac489
Tiago Nobrega <tiago.nobrega@canonical.com>
2026-01-22 14:41:44 -0300

    feat: disable uv and poetry in 25.10+
    These plugins need new, usrmerge-ready version. Now that we have plugin
    groups we can at least disable the plugins instead of having the lifecycle
    fail during build.

--- tests/unit/plugins/test_groups.py
+++ tests/unit/plugins/test_groups.py
@@ -18,7 +18,11 @@
 
 import pytest
 from craft_parts.plugins.dotnet_plugin import DotnetPlugin as DotnetPluginV1
+from craft_parts.plugins.python_v2.python_plugin import PythonPlugin as PythonPluginV2
 from rockcraft.plugins.groups import get_plugin_group
+from rockcraft.plugins.poetry_plugin import PoetryPlugin as PoetryPluginV1
+from rockcraft.plugins.python_plugin import PythonPlugin as PythonPluginV1
+from rockcraft.plugins.uv_plugin import UvPlugin as UvPluginV1
 
 LEGACY_BASES = ["ubuntu@20.04", "ubuntu@22.04", "ubuntu@24.04"]
 DEFAULT_BASES = ["ubuntu@25.10", "devel", "ubuntu@devel"]
@@ -34,3 +38,20 @@ def test_legacy_groups_dotnet(legacy_base):
 def test_default_dotnet(base):
     group = get_plugin_group(base)
     assert "dotnet" not in group
+
+
+@pytest.mark.parametrize("legacy_base", LEGACY_BASES)
+def test_legacy_groups_python(legacy_base):
+    group = get_plugin_group(legacy_base)
+    assert group["python"] is PythonPluginV1
+    assert group["poetry"] is PoetryPluginV1
+    assert group["uv"] is UvPluginV1
+
+
+@pytest.mark.parametrize("base", DEFAULT_BASES)
+def test_default_python(base):
+    group = get_plugin_group(base)
+    assert group["python"] is PythonPluginV2
+    # No v2 of the poetry and uv plugins yet
+    assert "poetry" not in group
+    assert "uv" not in group
