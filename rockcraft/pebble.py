f8c8d55b8ad5739a87d45887f07d222800f54121
smethnani <82812166+smethnani@users.noreply.github.com>
2026-05-26 09:11:04 +0100

    fix: use fips channel when fips services enabled

--- rockcraft/pebble.py
+++ rockcraft/pebble.py
@@ -18,6 +18,7 @@
 
 import enum
 from collections.abc import Mapping
+from copy import deepcopy
 from pathlib import Path
 from typing import Annotated, Any, Literal
 
@@ -26,6 +27,7 @@ import pydantic
 import yaml
 from craft_application.errors import CraftValidationError
 from craft_application.models import CraftBaseModel
+from craft_application.util import ProServices
 from craft_cli import emit
 
 
@@ -496,7 +498,14 @@ def add_pebble_part(project: dict[str, Any]) -> None:
         # don't try to add the relevant pebble part if base is missing
         # (friendly error message will be presented later from pydantic)
         return
-    pebble_part = Pebble.get_part_spec(build_base)
+    pebble_part = deepcopy(Pebble.get_part_spec(build_base))
+
+    if ProServices._get_pro_services() & {  # noqa: SLF001 # pyright: ignore[reportPrivateUsage]
+        "fips",
+        "fips-preview",
+        "fips-updates",
+    }:
+        pebble_part["stage-snaps"] = ["pebble/fips/stable"]
 
     parts = project["parts"]
     if "pebble" in parts:
